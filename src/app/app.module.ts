89dd4ae8a81960727b72fd78f5cb1ef3ffb1f404
theprogrammer <alliemoosa08@gmail.com>
2022-11-15 22:36:28 +0200

    This commit contains the following changes:
    - Adding a logo.
    - Adding headers.
    - Adding footer, and home components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { NavbarComponent } from './common-component/navbar/navbar.component';
+import { FooterComponent } from './common-component/footer/footer.component';
+import { HomeComponent } from './common-component/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
