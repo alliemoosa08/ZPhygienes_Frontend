87e2d44449f2cb56c359c75e81e59edef9a2bad1
theprogrammer <alliemoosa08@gmail.com>
2023-04-12 21:39:59 +0200

    First completion of `our service` screen.

--- src/app/our-service/our-service.component.ts
+++ src/app/our-service/our-service.component.ts
@@ -12,4 +12,22 @@ export class OurServiceComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  goToCoucheService(){
+  console.log("couche service works")
+  }
+  goToRugService(){
+    console.log("Rug service works")
+  }
+  goToSneakerService(){
+    console.log("Sneaker service works")
+  }
+  goToCarIntService(){
+    console.log("Car service works")
+  }
+  goToOfficeService(){
+    console.log("Office service works")
+  }
+  goToHomeService(){
+    console.log("Home service works")
+  }
 }
