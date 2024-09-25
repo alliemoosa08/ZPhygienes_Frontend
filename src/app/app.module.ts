090c24852eb7a26f4d3042520e26cb3367cfeb96
moosaa <moosaa@detnet.com>
2024-09-25 16:41:22 +0200

    Modify navbar to be responsive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { MatCardModule } from '@angular/material/card';
-import { HomeComponent } from './home/home.component'
 import { NavbarComponent } from './common-component/navbar/navbar.component';
-import { OurServiceComponent } from './our-service/our-service.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-
+import { HomeComponent } from './home/home.component';
+import { OurServiceComponent } from './our-service/our-service.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,10 +29,13 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     BrowserAnimationsModule,
     MatDividerModule,
     MatToolbarModule,
-    MatCardModule
-
+    MatCardModule,
+    MatIconModule,
+    MatSidenavModule 
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/common-component/navbar/navbar.component.ts
+++ src/app/common-component/navbar/navbar.component.ts
@@ -8,41 +8,52 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   showToggler: boolean = false;
-
-
+  menuOpened = false;
+  events: string[] = [];
+  opened: boolean = false;
   constructor(
     private router: Router
-  ) { 
+  ) {
     this.checkScreenSize();
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
+    console.log(event)
     this.checkScreenSize();
   }
 
   checkScreenSize() {
-    this.showToggler = window.innerWidth < 426; // Show toggler on mobile screens
+    this.showToggler = window.innerWidth < 430; // Show toggler on mobile screens
   }
 
   ngOnInit(): void {
   }
+
   home() {
+    this.menuOpened = !this.menuOpened
     this.router.navigate(["/home"])
   }
 
   ourServices() {
+    this.menuOpened = !this.menuOpened
     this.router.navigate(["/our-services"])
   }
   aboutUs() {
+    this.menuOpened = !this.menuOpened
     this.router.navigate(["/about-us"])
   }
   contactUs() {
+    this.menuOpened = !this.menuOpened
     this.router.navigate(["/contact-us"])
   }
 
   signIn() {
+    this.menuOpened = !this.menuOpened
     this.router.navigate(["/sign-in"])
   }
 
+  isMenuOpen() {
+   this.menuOpened = !this.menuOpened
+  }
 }
