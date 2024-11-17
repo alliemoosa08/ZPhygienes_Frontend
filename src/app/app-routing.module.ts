56a303bb766b0f527fb374b8891e3f8171e8a2c8
moosaa <moosaa@detnet.com>
2024-11-17 22:22:18 +0200

    Adding couch services pictures.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
+import { CarSeatComponent } from './our-service/car-seat/car-seat.component';
+import { ChairComponent } from './our-service/chair/chair.component';
+import { CouchComponent } from './our-service/couch/couch.component';
+import { HouseComponent } from './our-service/house/house.component';
+import { MattressComponent } from './our-service/mattress/mattress.component';
+import { OfficeComponent } from './our-service/office/office.component';
 import { OurServiceComponent } from './our-service/our-service.component';
+import { RugsComponent } from './our-service/rugs/rugs.component';
 
 const routes: Routes = [
   //Navigating to navBar pages
@@ -11,7 +18,16 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'our-services', component: OurServiceComponent },
   { path: 'about-us', component: AboutUsComponent },
-  { path: 'contact-us', component: ContactUsComponent }
+  { path: 'contact-us', component: ContactUsComponent },
+
+  //Our services pages
+  { path: 'our-services/couch', component: CouchComponent },
+  { path: 'our-services/chair', component: ChairComponent },
+  { path: 'our-services/rugs', component: RugsComponent },
+  { path: 'our-services/mattress', component: MattressComponent },
+  { path: 'our-services/office', component: OfficeComponent },
+  { path: 'our-services/house', component: HouseComponent },
+  { path: 'our-services/car-seat', component: CarSeatComponent },
 ];
 
 @NgModule({
--- src/app/our-service/couch/couch.component.ts
+++ src/app/our-service/couch/couch.component.ts
@@ -1,14 +1,113 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-couch',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CommonModule],
   templateUrl: './couch.component.html',
   styleUrl: './couch.component.css'
 })
-export class CouchComponent {
+export class CouchComponent implements OnInit {
+
+
+  couchServices: any[] = [
+    {
+      id: 1,
+      title: '1 Seater',
+      image: '/assets/services/couch/1-seater.jpg',
+      description: 'Deep cleaning for 1 seater',
+      price: "R200.00"
+    },
+    {
+      id: 2,
+      title: '2 Seater',
+      image: '/assets/services/couch/2-seater.jpg',
+      description: 'Deep cleaning for 1 seater',
+      price: "R300.00"
+    },
+    {
+      id: 3,
+      title: '2 Seater L shape',
+      image: '/assets/services/couch/2-L-seater.jpg',
+      description: 'View list of couch service',
+      price: "R350.00" 
+    },
+    {
+      id: 4,
+      title: '3 Seater',
+      image: '/assets/services/couch/3-seater.jpg',
+      description: 'View list of couch service',
+      price: "R450.00"
+    },
+    {
+      id: 5,
+      title: '3 Seater L shape',
+      image: '/assets/services/couch/3-L-seater.jpg',
+      description: 'View list of couch service',
+      price: "R500.00"
+    },
+    {
+      id: 6,
+      title: '4 Seater',
+      image: '/assets/services/couch/4-seater.jpg',
+      description: 'View list of couch service',
+      price: "R550.00"
+    },
+    {
+      id: 7,
+      title: '4 Seater L shape',
+      image: '/assets/services/couch/4-L-seater.jpg',
+      description: 'View list of couch service',
+      price: "R600.00"
+    },
+    {
+      id: 8,
+      title: '5 Seater',
+      image: '/assets/services/couch/5-seater.jpg',
+      description: 'View list of couch service',
+      price: "R650.00"
+    },
+    {
+      id: 9,
+      title: '5 Seater L shape',
+      image: '/assets/services/couch/5-L-seater.jpg',
+      description: 'View list of couch service',
+      price: "R700.00"
+    },
+    {
+      id: 10,
+      title: '6 Seater',
+      image: '/assets/services/couch/6-seater.jpg',
+      description: 'View list of couch service',
+      price: "R750.00"
+    },
+    {
+      id: 11,
+      title: '6 Seater L shape',
+      image: '/assets/services/couch/6-L-seater.jpg',
+      description: 'View list of couch service',
+      price: "R800.00"
+    }
+  ]
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  addToWishlist(id: number) {
+    console.log('Service added to wishlist with ID:', id);
+  }
+
+  addToBooking(id: number) {
+    console.log('Service added to booking with ID:', id);
+  }
 
 }
